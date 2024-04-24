ee43eb71699f5560a12c199736bf87a685573639
Nilson Jorge Leão Júnior <116318343+NilsonJorge@users.noreply.github.com>
2024-04-24 17:04:29 -0300

    miniblog4

---
+++ 10_MINIBLOG/miniblog/src/hooks/useFetchDocuments.jsx
@@ -0,0 +1,44 @@
+import {useState, useEffect} from 'react';
+import {db} from '../firebase/config';
+import {collection,query,onSnapshot,where, orderBy, getDocs} from 'firebase/firestore' 
+ 
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+ 
+    const [documents, setDocuments] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(null);
+ 
+    // deal with memory leak
+    const [cancelled, setCancelled] = useState(false)
+ 
+        useEffect(() => {
+        
+            const getUsers = async() => {
+            
+            if(cancelled) return 
+ 
+            setLoading(true);
+ 
+            const userCollectionRef = collection(db,docCollection);
+            
+            try {
+                let q ;
+                    q= await getDocs(userCollectionRef);
+                    setDocuments( q.docs.map((doc) => ({
+                        ...doc.data(),
+                        id:doc.id,
+                    })))
+                console.log(documents)
+                setLoading(false)
+            } catch (error) {
+                setLoading(false)
+            }
+ 
+        }
+        getUsers();
+    },[])
+    useEffect(()=> {
+        return () => setCancelled(true)
+    },[]);
+    return {documents,loading,error};
+}
