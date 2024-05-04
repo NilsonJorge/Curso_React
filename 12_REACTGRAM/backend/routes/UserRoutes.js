45a87fbc6354c4fc1c98050089ae827de5ff5309
Nilson Jorge Leão Júnior <116318343+NilsonJorge@users.noreply.github.com>
2024-05-04 16:32:09 -0300

    back_reactgram

--- 12_REACTGRAM/backend/routes/UserRoutes.js
+++ 12_REACTGRAM/backend/routes/UserRoutes.js
@@ -3,14 +3,18 @@ const router = express.Router()
 
 
 //Controller
-const {register, login} = require("../controllers/UserController");
+const {register, login, getCurrentUser, update} = require("../controllers/UserController");
 
 //Middlewares
 const validate = require("../middlewares/handleValidation");
-const {userCreateValidation, loginValidation} = require("../middlewares/userValidation")
+const {userCreateValidation, loginValidation, userUpdateValidation} = require("../middlewares/userValidation")
+const authGuard = require("../middlewares/authGuard");
+const { imageUpload } = require("../middlewares/imageUpload");
 
 //Routes
 router.post('/register', userCreateValidation(), validate, register);
 router.post('/login', loginValidation(), validate, login);
+router.get("/profile", authGuard, getCurrentUser);
+router.put("/", authGuard, userUpdateValidation(), validate, imageUpload.single("profileImage"), update)
 
 module.exports = router;
