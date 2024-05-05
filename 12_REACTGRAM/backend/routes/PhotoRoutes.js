2d0a53d9b50d466d7f13cd4b8aca7a656698086f
Nilson Jorge Leão Júnior <116318343+NilsonJorge@users.noreply.github.com>
2024-05-05 18:35:05 -0300

    back_reactgram

--- 12_REACTGRAM/backend/routes/PhotoRoutes.js
+++ 12_REACTGRAM/backend/routes/PhotoRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router()
 
 //Controller
-const {insertPhoto, deletePhoto, getAllPhotos} = require("../controllers/PhotoController")
+const {insertPhoto, deletePhoto, getAllPhotos, getUserPhotos, getPhotoById, updatePhoto, likePhoto, commentPhoto} = require("../controllers/PhotoController")
 
 //Middlewares
-const {photoInsertValidation} = require("../middlewares/photoValidation");
+const {photoInsertValidation, photoUpdateValidation} = require("../middlewares/photoValidation");
 const authGuard = require("../middlewares/authGuard");
 const validate = require("../middlewares/handleValidation");
 const {imageUpload} = require("../middlewares/imageUpload")
@@ -15,5 +15,11 @@ const {imageUpload} = require("../middlewares/imageUpload")
 router.post("/", authGuard, imageUpload.single('image'), photoInsertValidation(), validate, insertPhoto);
 router.delete("/:id", authGuard, deletePhoto)
 router.get("/", authGuard, getAllPhotos);
+router.get("/user/:id", authGuard, getUserPhotos);
+router.get("/:id", authGuard, getPhotoById);
+router.put("/:id",authGuard, photoUpdateValidation(), validate, updatePhoto)
+router.put("/like/:id", authGuard, likePhoto)
+router.put("/comment/:id", authGuard, commentPhoto)
+
 
 module.exports = router;
