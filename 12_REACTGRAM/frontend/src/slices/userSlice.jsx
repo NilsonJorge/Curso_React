9808e888612f606772054b6da1b2847b3c7ddb06
Nilson Jorge Leão Júnior <116318343+NilsonJorge@users.noreply.github.com>
2024-05-13 17:00:17 -0300

    prof

--- 12_REACTGRAM/frontend/src/slices/userSlice.jsx
+++ 12_REACTGRAM/frontend/src/slices/userSlice.jsx
@@ -35,6 +35,15 @@ export const updateProfile = createAsyncThunk(
         }
         return data;
     }
+);
+
+//Get user details
+export const getUserDetails = createAsyncThunk(
+    "user/get",
+    async(id, thunkAPI) => {
+        const data = await userService.getUserDetails(id);
+        return data;
+    }
 )
 
 
@@ -74,6 +83,16 @@ export const userSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
             state.user = {};
+        })
+        .addCase(getUserDetails.pending, (state) => {
+            state.loading = true;
+            state.error = false;
+        })
+        .addCase(getUserDetails.fulfilled, (state, action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.user = action.payload;
         });
     },
 });
